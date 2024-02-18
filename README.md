ea280b1f4a27a01b6f1ac7849b41a3b09c88e6c9
Ezharjan <ezharjan@outlook.com>
2024-02-18 13:13:18 +0800

    revised styles

--- README.md
+++ README.md
@@ -1,17 +1,20 @@
-# db-meter
+# [DetectDB](https://ezharjan.github.io/detectdb)
 
-https://takispig.github.io/db-meter/<br><br>
-Simple Webinterface to estimate the noise level in Decibels.  
-Values are not precise, but mainly represent differencies in noise levels in a short period of time.
-
-![Website](/demo.png)
+A simple web interface is available for estimating the noise level in decibels. The values provided are not precise, but they primarily indicate variations in noise levels over a short period of time.
 
 ## Algorithm
-In this project I tried to get the noise level for different frequencies, take the average out of them, and then again take the average of all such values within the time intervall of a specific refresh_rate (in msec). Afterwards I applied the formula $20*log10(noiseValue)$ to get a number indication near to Decibel, and added the offset value.  
+The algorithm employed in this project involves calculating the noise level for different frequencies, calculating the average of those values, and then determining the average of all the values within a specific time interval known as the refresh rate (measured in milliseconds). The formula used to obtain a number approximation close to decibels is $20*log10(noiseValue)$, with an additional offset value added.
 
 ## Offset & Refresh Rate
-To cope with the environmental noises and different microphone sensitivities, I have set an adjustable offset value, which just increases or decreases the dBs. As default, the value of 30 has been chosen, as it yields the best results (at least for me).  
-Furthermore, the refresh rate for the dBs is adjustable too (default 1 sec).  
+To account for environmental noises and variations in microphone sensitivities, an adjustable offset value has been implemented. This offset value can increase or decrease the decibel measurements. By default, a value of 30 has been selected, as it has been found to produce the most satisfactory results. Additionally, the refresh rate for the decibel measurements can be adjusted, with a default setting of 1 second.
 
 ## Technnical Details
-For the Webinterface has been used HTML5, CSS and JavaScript. The code for the Decibel estimation has been written in JavaScript with Web Audio API, and more specific with the AudioContext Interface.
+The web interface utilizes HTML5, CSS, and JavaScript. The JavaScript code responsible for estimating decibels is implemented using the Web Audio API, specifically the AudioContext Interface.
+
+_[OriginalRepo](https://github.com/takispig/db-meter)_
+
+<br>
+<br>
+<br>
+
+*Slight Changes have been made in this repository.*
--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-let localDbValues = [];     // array to store db values for each loop withing the refresh_rate
+let biasDB = 30;
+let visualsWidth = 0.3;
+let localDbValues = []; // array to store db values for each loop withing the refresh_rate
 let refresh_rate = 500;
-let color = 'green';
+let color = "green";
 let stream;
 let offset = 0;
 let date;
 
-navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then((stream) => {
-    
+navigator.mediaDevices
+  .getUserMedia({ audio: true, video: false })
+  .then((stream) => {
     const context = new AudioContext();
     const source = context.createMediaStreamSource(stream);
     const processor = context.createScriptProcessor(2048, 1, 1);
@@ -20,80 +23,82 @@ navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then((stream)
     processor.connect(context.destination);
 
     processor.onaudioprocess = () => {
+      let data = new Uint8Array(analyser.frequencyBinCount);
+      analyser.getByteFrequencyData(data);
+      let rms = 0;
 
-        let data = new Uint8Array(analyser.frequencyBinCount);
-        analyser.getByteFrequencyData(data);
-        let rms = 0;
+      for (var i = 0; i < data.length; i++) {
+        if (data[i] > 120) data[i] = 120;
+        rms += data[i] * data[i];
+      }
+      rms = Math.sqrt(rms / data.length);
+      //   console.log("RMS: " + rms);
 
-        for (var i = 0; i < data.length; i++) {
-            if (data[i]>120) data[i] = 120
-            rms += data[i]*data[i]
-        }
-        rms = Math.sqrt(rms / data.length);
-        console.log("RMS: " + rms)
-
-        offset = parseInt(document.getElementById("offset").value);
-        document.getElementById("offset_value").innerText = offset;
-        value = rms + offset;
-        localDbValues.push(value);
+      offset = parseInt(document.getElementById("offset").value);
+      document.getElementById("offset_value").innerText = offset;
+      value = rms + offset + biasDB;
+      localDbValues.push(value);
     };
-})
+  });
 
 // update the volume every refresh_rate m.seconds
-let updateDb = function() {
-    window.clearInterval(interval);
+let updateDb = function () {
+  window.clearInterval(interval);
 
-    const db = document.getElementById("db");
-    let volume = Math.round(localDbValues.reduce((a,b) => a+b) / localDbValues.length);
-    //var volume = Math.round(Math.max.apply(null, localDbValues));
-    if (!isFinite(volume)) volume = 0;  // we don't want/need negative decibels in that case
-    db.innerText = volume;
-    localDbValues = []; // clear previous values
-    changeColor(volume);
+  const db = document.getElementById("db");
+  let volume = Math.round(
+    localDbValues.reduce((a, b) => a + b) / localDbValues.length
+  );
+  //var volume = Math.round(Math.max.apply(null, localDbValues));
+  if (!isFinite(volume)) volume = 0; // we don't want/need negative decibels in that case
+  db.innerText = volume;
+  localDbValues = []; // clear previous values
+  changeColor(volume);
 
-    changeUpdateRate();
-    interval = window.setInterval(updateDb, refresh_rate);
-}
+  changeUpdateRate();
+  interval = window.setInterval(updateDb, refresh_rate);
+};
 let interval = window.setInterval(updateDb, refresh_rate);
-
 
 // change the visualization colors according to the dbValue
 function changeColor(decibels) {
-    if (decibels < 50) {color = 'green'}
-    else if (decibels >=50 && decibels <70) {color = 'yellow'}
-    else if (decibels >= 70 && decibels <90) {color = 'orange'}
-    else {color = 'red'}
+  if (decibels < 50) {
+    color = "green";
+  } else if (decibels >= 50 && decibels < 70) {
+    color = "yellow";
+  } else if (decibels >= 70 && decibels < 90) {
+    color = "orange";
+  } else {
+    color = "red";
+  }
 
-    //document.getElementById("visuals").style.height = dbValue + "px";
-    document.getElementById("visuals").style.width = decibels*2/10 + "rem";
-    if (decibels>=70) document.getElementById("visuals").style.background = 'red';
-    else document.getElementById("visuals").style.background = 'black';
-    document.getElementById("db").style.color = color;
+  //document.getElementById("visuals").style.height = dbValue + "px";
+  document.getElementById("visuals").style.width = (decibels * 2) * visualsWidth + "rem";
+  if (decibels >= 70) {
+    document.getElementById("visuals").style.background = "red";
+  } else {
+    let modeColorStr = document.body.classList.contains("dark-mode") && "white" || "black"
+    document.getElementById("visuals").style.background = modeColorStr;
+  }
+  document.getElementById("db").style.color = color;
 }
-
 
 // change update rate
 function changeUpdateRate() {
-    refresh_rate = Number(document.getElementById("refresh_rate").value);
-    document.getElementById("refresh_value").innerText = refresh_rate;
-    window.setInterval(function() {
-        updateDb;
-    }, refresh_rate);
+  refresh_rate = Number(document.getElementById("refresh_rate").value);
+  document.getElementById("refresh_value").innerText = refresh_rate;
+  window.setInterval(function () {
+    updateDb;
+  }, refresh_rate);
 }
 
-
-// update the date of last project's version
-let xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    let repos = JSON.parse(this.responseText);
-    repos.forEach((repo)=>{
-        if (repo.name == "db-meter") {
-            date = new Date(repo.pushed_at);
-            document.getElementById("date").innerText = date.toLocaleDateString();
-        }
-    });
+function toggleBtnMode() {
+  const body = document.body;
+  const button = document.getElementById("mode-toggle");
+  body.classList.toggle("dark-mode");
+  if (body.classList.contains("dark-mode")) {
+    button.textContent = "Light Mode";
+  } else {
+    button.textContent = "Dark Mode";
   }
-};
-xhttp.open("GET", "https://api.github.com/users/takispig/repos", true);
-xhttp.send();
+}
